29d88ea32d0fe80aab3fce9412e826e4739beea9
Quinntyne Brown <quinntynebrown@gmail.com>
2017-05-17 23:19:25 -0400

    add some code

---
+++ src/electron-getting-started/src/main.ts
@@ -0,0 +1,17 @@
+﻿import { app, BrowserWindow } from "electron";
+const path = require('path');
+const url = require('url');
+
+
+let win: Electron.BrowserWindow;
+
+function createWindow() {
+
+    win = new BrowserWindow({ width: 800, height: 600 });
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+}
